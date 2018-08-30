3a90ad8f43d824520a69f90b0db2d64267fc1fba
Anderson <anderson16.yk@gmail.com>
2018-08-30 20:19:23 -0300

    adicionando templaytes

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateField(default=timezone.now)
@@ -12,7 +14,5 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-    def __str__ (self):
+    def __str__(self):
         return self.title
-
-
